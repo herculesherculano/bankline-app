650550abbb14f7645772ddb8350bae62e7e8f729
herculesherculano <hbheroi@gmail.com>
2022-06-01 00:42:08 -0300

    modificada a baseUrl para uso com heroku

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -3,7 +3,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://hercules-dio-bankline-api.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
